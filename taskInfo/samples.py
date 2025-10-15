d55b9a1b9750da90c08c645020d3585c86f806ec
Diyor-S <ruthlessfatality@gmail.com>
2025-10-15 20:12:24 +0500

    updated samples by stating checked for 3x3 as well

--- taskInfo/samples.py
+++ taskInfo/samples.py
@@ -14,4 +14,19 @@ A = [
 
 X - the vector we are looking for. Directly related to NoU (the number of unknows) that we should find.
 B - column vector, results we obtain based on the x.
+
+    Also solved for the 3x3 in IndependentWork, also gave True
+
+    print_initial_state(A_3, B_3)
+
+    X = calculate_matrix(A_3, B_3)
+    print(f"My solution:\n{np.around(X, decimals=16)}")
+
+    X_official = np.linalg.solve(A_3, B_3)
+    is_solution_correct = np.allclose(X, X_official)
+
+    print(f"Official Solution:\n{X_official}")
+    print(f"Your Solution Matches Official: {is_solution_correct}")
+
+
 """
