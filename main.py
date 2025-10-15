7b95ca1f328b816e39635545fcbf85122bbc4533
Diyor-S <ruthlessfatality@gmail.com>
2025-10-15 22:27:39 +0500

    refactored, reformated

--- main.py
+++ main.py
@@ -8,19 +8,21 @@ def _main():
     print_initial_state(A, B)
 
     (X, det_A), symmetrized_A = calculate_matrix(A, B)
-    print(f"My solution:\n{np.around(symmetrized_A, decimals=16)}")
-    print(f"My solution:\n{np.around(det_A, decimals=16)}")
-    print(f"My solution:\n{np.around(X, decimals=16)}")
+    print("\n\nMy solution: \n")
+    print(f"Symmetrized Matrix A:\n{np.around(symmetrized_A, decimals=16)}")
+    print(f"Determinant of Symmetrized A:\n{np.around(det_A, decimals=16)}")
+    print(f"Final Result X:\n{np.around(X, decimals=16)}")
 
     X_official = np.linalg.solve(A, B)
     det_A_official = np.linalg.det(symmetrized_A)
     det_A_official_orig = np.linalg.det(A)
     is_solution_correct = np.allclose(X, X_official)
 
-    print(f"Official Solution:\n{X_official}")
-    print(f"Official Solution:\n{det_A_official}")
-    print(f"Official Solution:\n{det_A_official_orig}")
-    print(f"Your Solution Matches Official: {is_solution_correct}")
+    print("\n\nOfficial solution:")
+    print(f"Determinant of symmetrized A:\n{det_A_official}")
+    print(f"Determinant of original Matrix A:\n{det_A_official_orig}")
+    print(f"Solution for X:\n{X_official}")
+    print(f"\tDoes Solution Match Official: {is_solution_correct}")
 
 
 
