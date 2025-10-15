baa4d948be98986544a802f24f2a9914f76bc597
Diyor-S <ruthlessfatality@gmail.com>
2025-10-15 22:14:56 +0500

    added print det A as well to the solution

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ import numpy as np
 def _main():
     print_initial_state(A, B)
 
-    X = calculate_matrix(A, B)
+    (X, det_A), symmetrized_A = calculate_matrix(A, B)
+    print(f"My solution:\n{np.around(symmetrized_A, decimals=16)}")
+    print(f"My solution:\n{np.around(det_A, decimals=16)}")
     print(f"My solution:\n{np.around(X, decimals=16)}")
 
     X_official = np.linalg.solve(A, B)
+    det_A_official = np.linalg.det(symmetrized_A)
+    det_A_official_orig = np.linalg.det(A)
     is_solution_correct = np.allclose(X, X_official)
 
     print(f"Official Solution:\n{X_official}")
+    print(f"Official Solution:\n{det_A_official}")
+    print(f"Official Solution:\n{det_A_official_orig}")
     print(f"Your Solution Matches Official: {is_solution_correct}")
 
 
