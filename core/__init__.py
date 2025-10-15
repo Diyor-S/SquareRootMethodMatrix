2169b0c58d50a10cb4673ec21d7288e4d1e2470e
Diyor-S <ruthlessfatality@gmail.com>
2025-10-15 22:15:16 +0500

    exported the det calc function for convenience

--- core/__init__.py
+++ core/__init__.py
@@ -3,6 +3,7 @@ __all__ = [
     "cheolesky_factorization",
     "forward_substitution",
     "backward_substitution",
+    "calculate_determinant_of_symmetrized_A",
 ]
 
 
@@ -10,3 +11,4 @@ from .symmetrization import symmetrize
 from .cheolesky_factorization import cheolesky_factorization
 from .forward_substitution import forward_substitution
 from .backward_substitution import backward_substitution
+from .det_symmetric_matrix import calculate_determinant_of_symmetrized_A
