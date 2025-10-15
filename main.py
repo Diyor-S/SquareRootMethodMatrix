4bb5123f270a2409b501756a4492987f1e471ffd
Diyor-S <ruthlessfatality@gmail.com>
2025-10-15 20:13:05 +0500

    the main call for the calculation func happens here

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+from global_v import A, B  # A_3, B_3
+from utils import print_initial_state, calculate_matrix
+
+import numpy as np
 
 
-def entry_point():
-    pass
+def _main():
+    print_initial_state(A, B)
+
+    X = calculate_matrix(A, B)
+    print(f"My solution:\n{np.around(X, decimals=16)}")
+
+    X_official = np.linalg.solve(A, B)
+    is_solution_correct = np.allclose(X, X_official)
+
+    print(f"Official Solution:\n{X_official}")
+    print(f"Your Solution Matches Official: {is_solution_correct}")
 
 
-if __name__ == "__main__":
-    entry_point()
+
